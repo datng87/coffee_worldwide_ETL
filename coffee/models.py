c3b748adc86455fc4ef62ff07ee4b3a1f9f4c66a
datng87 <dat.q.nguyen87@gmail.com>
2022-09-10 16:43:01 +1000

    finished

--- coffee/models.py
+++ coffee/models.py
@@ -3,9 +3,9 @@ from .app import db
 
 class coffee(db.Model):
     __tablename__ = 'coffee'
-    Country_Code = db.Column(db.String(128),primary_key=True) 
+    Country_Code = db.Column(db.String(128)) 
     Country_Name = db.Column(db.String(128),primary_key=True) 
-    Market_Year = db.Column(db.Integer) 
+    Market_Year = db.Column(db.Integer,primary_key=True) 
     Arabica_Production = db.Column(db.Integer) 
     Robusta_Production = db.Column(db.Integer) 
     Other_Production = db.Column(db.Integer) 
